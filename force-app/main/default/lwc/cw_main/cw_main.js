8d794a70f9e86914dbc6e5d3d8add13d4eb33045
marticarrSFDC <86790148+marticarrSFDC@users.noreply.github.com>
2024-10-23 15:30:01 -0400

    clean up

--- force-app/main/default/lwc/cw_main/cw_main.js
+++ force-app/main/default/lwc/cw_main/cw_main.js
@@ -4,25 +4,6 @@ import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
 import createGoogleSheet from '@salesforce/apex/CW_GoogleSheetsService.createGoogleSheet';
 
 export default class Cw_main extends NavigationMixin(LightningElement) {
-    currentPageReference = null; 
-    urlStateParameters = null;
-    code = null;
-
-    @wire(CurrentPageReference)
-    getStateParameters(currentPageReference) {
-        console.log(JSON.stringify(currentPageReference));
-        if (currentPageReference) {
-            this.urlStateParameters = currentPageReference.state;
-            this.setParametersBasedOnUrl();
-        }
-    }
-
-    setParametersBasedOnUrl() {
-        this.code = this.urlStateParameters.code || null;
-        console.log(JSON.stringify(this.urlStateParameters));
-        console.log(this.code);
-    }
-
     handleClick(event) {
         createGoogleSheet()
         .then(result => {
